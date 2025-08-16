b8d1e7d3ae08c3104d99c40c17ce9705261db150
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-08-16 17:54:55 +0530

    added all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import { Footer } from "./components/footer/Footer"
+import { Header } from "./components/header/Header"
+import { Home } from "./pages/home/Home"
+import { Login } from "./pages/login/Login"
+import { Regsiter } from "./pages/login/Regsiter"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { DetailsPages } from "./pages/details/DetailsPages"
+import { Account } from "./pages/account/Account"
+import { Create } from "./components/create/Create"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Regsiter} />
+          <Route exact path='/details/:id' component={DetailsPages} />
+          <Route exact path='/account' component={Account} />
+          <Route exact path='/create' component={Create} />
+        </Switch>
+        <Footer />
+      </Router>
     </>
   )
 }
-
 export default App
